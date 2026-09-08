b02a1928d4fba7c9829c1224b1f005f2c35edbe6
DROYER Florent <flo.droyer@gmail.com>
2026-09-08 08:26:55 +0200

    feat(ui): load responsive presentation layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './ui/responsive.css'
 import { App } from './app/App'
 import { AppProviders } from './app/providers'
 
