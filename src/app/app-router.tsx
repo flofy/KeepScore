f083a7f4c93174f85cb895346c563699f793107d
DROYER Florent <flo.droyer@gmail.com>
2026-09-10 21:55:34 +0200

    fix(setup): remove setup back button

--- src/app/app-router.tsx
+++ src/app/app-router.tsx
@@ -15,7 +15,7 @@ export const router = createAppRouter({
     </RouteShell>
   ),
   setup: (
-    <RouteShell close>
+    <RouteShell showBack={false}>
       <GameSetupRoute />
     </RouteShell>
   ),
