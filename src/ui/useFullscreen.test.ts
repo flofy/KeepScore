d88c8e610a854d1ed344d6f6f1a520a466b74cea
DROYER Florent <flo.droyer@gmail.com>
2026-09-13 16:02:55 +0200

    test(ui): remove low-value fullscreen hook test

--- src/ui/useFullscreen.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-import { useFullscreen } from "./useFullscreen";
-
-describe("useFullscreen", () => {
-  it("exports the fullscreen controller", () => {
-    expect(useFullscreen).toBeTypeOf("function");
-  });
-});
